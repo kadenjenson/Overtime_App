7815c6c58093c8194229ea8d4ac3ed6097e6e3ea
kadenjenson <sobeit99@gmail.com>
2017-11-09 11:28:55 -0700

    Added resource for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "Creation" do 
     before do 
-      @user = User.create(email: "test2@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "SpongeBob", last_name: "Sqaurepants")
+      @user = User.create(email: "test2@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "SpongeBob", last_name: "Squarepants")
     end
 
       it "can be created" do 
