0a87f2efce959e9395be4b99039fa9dcc3d05ce8
kadenjenson <sobeit99@gmail.com>
2017-11-28 16:17:13 -0700

    Blocked non admins from audit log dashboard

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -19,8 +19,14 @@ describe 'AuditLog Feature' do
       expect(page).to have_content(/SQUAREPANTS/)
     end
 
-    xit 'cannot be accessed by non admins' do
+    it 'cannot be accessed by non admins' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
 
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
     end
   end
 end
