4d14a510820e8fa24956fd1a5e06fffefca3ef38
kadenjenson <sobeit99@gmail.com>
2017-11-29 14:08:52 -0700

    Brought in live data for homepage and further styled views

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ AdminUser.create(email: "admin@test.com",
 puts "1 Admin created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 posts have been created"
